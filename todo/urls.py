5a528ebd143c06efb04e1f2a2e82e0201fdc85a5
atharva-developer0191 <patharva2003@gmail.com>
2025-07-17 23:58:27 +0530

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    # Add Task
     path('addTask/', views.addTask, name='addTask'),
+    # Mark Task as Done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # Mark Task as Undone
+    path('mark_as-undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # Edit Task
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # # Delete Task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,16 +1,50 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from todo.models import Task
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.filter(is_completed=False)
+
+    completed_tasks = Task.objects.filter(is_completed=True)
+
     context ={
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'home.html', context)
 
 def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,   
+        }
+        return render(request, 'edit_task.html', context)        
+    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
